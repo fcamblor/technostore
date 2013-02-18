ede2133075783f1a22efde0bab8efd5f9cda62bd
Frédéric Camblor <fcamblor@gmail.com>
2013-02-19 00:01:34 +0100

    declared require-handlebars-plugin + underscore dependencies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,34 @@
 require.config({
     paths: {
         jquery: '../components/jquery/jquery',
+        underscore: '../components/underscore/underscore',
+        json2: '../components/require-handlebars-plugin/hbs/json2',
+        handlebars: '../components/require-handlebars-plugin/Handlebars',
+        i18nprecompile: '../components/require-handlebars-plugin/hbs/i18nprecompile',
+        hbs: '../components/require-handlebars-plugin/hbs',
         bootstrap: 'vendor/bootstrap'
     },
     shim: {
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        underscore: {
+            deps: [],
+            exports: '_'
         }
+    },
+    // hbs particular configuration properties
+    hbs: {
+        disableI18n: true // Support for i18n is useless for the moment...
     }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+require(['hbs'], function () {
     'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+    require(['app', 'jquery', 'bootstrap'], function (app, $) {
+        // use app here
+        console.log(app);
+        console.log('Running jQuery %s', $().jquery);
+    });
 });
