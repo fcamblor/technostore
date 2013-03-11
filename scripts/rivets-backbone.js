81d8b1ebf75a6c9cbf2f1c832387793f9f0dc4ea
Frédéric Camblor <fcamblor@gmail.com>
2013-03-11 11:34:48 +0100

    integrated backbone/underscore/rivetjs

---
+++ scripts/rivets-backbone.js
@@ -0,0 +1,42 @@
+//define(['rivets', 'backbone'], function(rivets, Backbone){
+
+    // CONFIGURE RIVETS.JS WITH BACKBONE.JS
+
+    rivets.configure({
+      adapter: {
+        subscribe: function(obj, keypath, callback) {
+          if (obj instanceof Backbone.Collection) {
+            obj.on('add remove reset', function () {
+              callback(obj[keypath])
+            });
+          } else {
+            obj.on('change:' + keypath, function (m, v) { callback(v) });
+          };
+        },
+        unsubscribe: function(obj, keypath, callback) {
+          if (obj instanceof Backbone.Collection) {
+            obj.off('add remove reset', function () {
+              callback(obj[keypath])
+            });
+          } else {
+            obj.off('change:' + keypath, function (m, v) { callback(v) });
+          };
+        },
+        read: function(obj, keypath) {
+          if (obj instanceof Backbone.Collection) {
+            return obj[keypath];
+          } else {
+            return obj.get(keypath);
+          };
+        },
+        publish: function(obj, keypath, value) {
+          if (obj instanceof Backbone.Collection) {
+            obj[keypath] = value;
+          } else {
+            obj.set(keypath, value);
+          };
+        }
+      }
+    });
+
+//});
