ef19c74628f30b313f4d1aef811811ef72a153d9
Frédéric Camblor <fcamblor@gmail.com>
2013-03-25 23:11:56 +0100

    displaying techno with colors !

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,12 @@
 /*global define */
-define("app", ['jquery', 'routers/MainRouter', 'bootstrap'], function ($, MainRouter) {
+define("app", ['jquery', 'routers/MainRouter', 'handlebars', 'bootstrap'], function ($, MainRouter, Handlebars) {
     'use strict';
 
     console.log('Running jQuery %s', $().jquery);
+
+    Handlebars.registerHelper("typeToClass", function(){
+        return this.type === 1?"tooling":"jsfmk";
+    });
 
     new MainRouter();
 
