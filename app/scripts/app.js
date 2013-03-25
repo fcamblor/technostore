5c22a93c2c454237214fbb636f5eb3c17f00b40d
Frédéric Camblor <fcamblor@gmail.com>
2013-03-25 22:45:18 +0100

    implemented first backbone router !

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,10 @@
 /*global define */
-define("app", ['jquery', 'bootstrap'], function ($) {
+define("app", ['jquery', 'routers/MainRouter', 'bootstrap'], function ($, MainRouter) {
     'use strict';
 
     console.log('Running jQuery %s', $().jquery);
+
+    new MainRouter();
 
     return '\'Allo \'Allo!';
 });
