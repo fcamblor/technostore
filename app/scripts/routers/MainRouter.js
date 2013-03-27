8ec0dc7475197bd03570b3383bc06ce8e2ed350b
Frédéric Camblor <fcamblor@gmail.com>
2013-03-27 08:02:18 +0100

    refactored urls to not have any hash inside (and allow to use anchors). See http://stackoverflow.com/questions/12081894/backbone-router-navigate-and-anchor-href & https://github.com/tbranyen/backbone-boilerplate/blob/master/app/main.js

--- app/scripts/routers/MainRouter.js
+++ app/scripts/routers/MainRouter.js
@@ -4,15 +4,35 @@ define(["jquery", "backbone", "underscore"], function($, Backbone, _){
 
     var MainRouterClass = Backbone.Router.extend({
         routes: {
-            "!/hello": "sayHello",
-            "!/listTechnos": "listTechnos"
+            "hello": "sayHello",
+            "listTechnos": "listTechnos"
         },
     
         initialize: function () {
             MainRouterClass.__super__.initialize.apply(this, arguments);
+
             // Starting urls handlings
             // See http://backbonejs.org/#Router
-            Backbone.history.start();
+            Backbone.history.start({ pushState: true, root: "/" });
+
+            $(document).on("click", "a[href]:not([data-bypass])", function(evt) {
+                // Get the absolute anchor href.
+                var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
+                // Get the absolute root.
+                var root = location.protocol + "//" + location.host + "/";
+
+                // Ensure the root is part of the anchor href, meaning it's relative.
+                if (href.prop.slice(0, root.length) === root) {
+                    // Stop the default event to ensure the link will not cause a page
+                    // refresh.
+                    evt.preventDefault();
+
+                    // `Backbone.history.navigate` is sufficient for all Routers and will
+                    // trigger the correct events. The Router's internal `navigate` method
+                    // calls this anyways.  The fragment is sliced from the root.
+                    Backbone.history.navigate(href.attr, true);
+                }
+            });
         },
     
         sayHello: function(){
