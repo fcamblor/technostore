72e12ca231049161b7103eae4790c7330aa61a41
Frédéric Camblor <fcamblor@gmail.com>
2013-06-24 12:11:08 +0200

    requirejs-ified project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,58 @@
+/*global require */
 require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
-        bootstrap: 'vendor/bootstrap'
+        underscore: '../bower_components/underscore/underscore',
+        backbone: '../bower_components/backbone/backbone',
+        json2: '../bower_components/require-handlebars-plugin/hbs/json2',
+        handlebars: '../bower_components/require-handlebars-plugin/Handlebars',
+        i18nprecompile: '../bower_components/require-handlebars-plugin/hbs/i18nprecompile',
+        hbs: '../bower_components/require-handlebars-plugin/hbs',
+        bootstrap: 'vendor/bootstrap' /*,
+        rivets: '../bower_components/rivets/dist/rivets'
+        */
     },
     shim: {
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        underscore: {
+            deps: [],
+            exports: '_'
+        },
+        // "default" backbone.js files are not AMD-compatible
+        // => We must not consider it as proper backbone dependency, this is why we add here
+        // shim config which provide window.Backbone as module dependency
+        backbone: {
+            deps: ["underscore", "jquery"],
+            // Note : This is strange that exports:"Backbone" is not enough
+            // for strange reasons, we have to confirm it with init function
+            // because otherwise, window.Backbone won't be provided as module dependency
+            exports: "Backbone"
+        },
+        handlebars: {
+            deps: ['underscore'],
+            exports: 'Handlebars'
+        /* },
+        rivets: {
+            deps: [],
+            exports: 'rivets'
+            */
         }
+    },
+    deps: [ 'hbs' ],
+    // hbs particular configuration properties
+    hbs: {
+        disableI18n: true // Support for i18n is useless for the moment...
     }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+
+require(['app', 'jquery', 'hbs!templates/hello', 'bootstrap'], function (app, $, helloTemplate) {
     'use strict';
     // use app here
     console.log(app);
+    console.log(helloTemplate({ world: "tout le monde" }));
     console.log('Running jQuery %s', $().jquery);
 });
