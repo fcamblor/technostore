cca4697fbeffe5731e74122c80e0305a305335db
Frédéric Camblor <fcamblor@gmail.com>
2013-06-25 20:59:03 +0200

    Setting Backbone special field "id" in Todo Model, calculated depending on mongodb's oid This special "id" field will be used during a save() to identify if current model is a new one or not (see Backbone.Model.isNew() implementation). The isNew() test will imply either POST <rootURL> (if new) or PUT <rootUrl>/id call when executing the save() method. + Replaced url property by urlRoot property in order to not override default Backbone.Model.url() implementation (used during save())
    Conflicts:
    	app/scripts/models/Todo.js

--- app/scripts/models/Todo.js
+++ app/scripts/models/Todo.js
@@ -4,10 +4,15 @@ define(["backbone", "underscore"], function(Backbone, _){
     var TodoClass = Backbone.Model.extend({
         defaults: {
         },
-        url: "https://api.mongolab.com/api/1/databases/todomvc/collections/todos?apiKey=fO6K0boLuhcPc1LvTg1bAiox6i2em1cW",
+        urlRoot: "https://api.mongolab.com/api/1/databases/todomvc/collections/todos?apiKey=fO6K0boLuhcPc1LvTg1bAiox6i2em1cW",
 
         initialize: function(attributes, options){
             TodoClass.__super__.initialize.call(this,attributes, options);
+            // Binding mongodb's _id to Backbone model's id, in order to make save()
+            // work on current model, when id is available
+            this.id = this.get("_id")?this.get("_id")['$oid']:null;
+            // Ensuring this.id is updated in case the _id field is updated (should not happen in real life, but who knows...)
+            this.bind("change:_id", function(){ this.id = this.get("_id")?this.get("_id")['$oid']:null; }, this);
         },
 
         isCompleted: function(){
