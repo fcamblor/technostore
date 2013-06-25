0ad115dcb14533166b43dfeae94f8bb658b42263
Frédéric Camblor <fcamblor@gmail.com>
2013-06-26 01:21:40 +0200

    fix PUT and DELETE urls on todos

--- app/scripts/models/Todo.js
+++ app/scripts/models/Todo.js
@@ -4,10 +4,23 @@ define(["backbone", "underscore"], function(Backbone, _){
     var TodoClass = Backbone.Model.extend({
         defaults: {
         },
-        urlRoot: "https://api.mongolab.com/api/1/databases/todomvc/collections/todos?apiKey=fO6K0boLuhcPc1LvTg1bAiox6i2em1cW",
+        url: function(){
+            if(this.isNew()){
+                return "https://api.mongolab.com/api/1/databases/todomvc/collections/todos?apiKey=fO6K0boLuhcPc1LvTg1bAiox6i2em1cW";
+            } else {
+                return "https://api.mongolab.com/api/1/databases/todomvc/collections/todos/"+this.id+"?apiKey=fO6K0boLuhcPc1LvTg1bAiox6i2em1cW";
+            }
+        },
 
         initialize: function(attributes, options){
             TodoClass.__super__.initialize.call(this,attributes, options);
+
+            // Weird hack fixing some special case where a new Todo is instanciated from a Todos collection
+            // and where the url is overriden by the Todos' (collection) url whereas it shouldn't when some Backbone.Model.url()
+            // has been overwritten in current class
+            // Without this, you won't have correct urls for PUT (save when current model is not new) and DELETE (destroy)
+            this.url = TodoClass.prototype.url;
+
             // Binding mongodb's _id to Backbone model's id, in order to make save()
             // work on current model, when id is available
             var updateModelId = function(){ this.id = this.get("_id")?this.get("_id")['$oid']:null; };
