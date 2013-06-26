16bbc26bcff536698a845ed832f2f260ac1f8f1c
Frédéric Camblor <fcamblor@gmail.com>
2013-06-26 02:06:02 +0200

    provided a custom rivets formatter allowing to display todo's timestamp as a humanly readable date

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -90,6 +90,15 @@ define(['routers/MainRouter', 'rivets', 'jquery'], function (MainRouter, rivets,
       }
     });
 
+    rivets.formatters['formatDate'] = function(timestamp){
+        if(timestamp){
+            var date = new Date(timestamp);
+            return date.getFullYear()+"/"+(date.getMonth()<9?"0":"")+(date.getMonth()+1)+"/"+(date.getDate()<10?"0":"")+date.getDate()
+                +" "+(date.getHours()<10?"0":"")+date.getHours()+":"+(date.getMinutes()<10?"0":"")+date.getMinutes();
+        } else {
+            return "";
+        }
+    };
 
     new MainRouter();
 
